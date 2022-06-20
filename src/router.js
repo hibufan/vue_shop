85029e5c38b9a0a0fbc830770e5d7f3ced3a6ddf
hibufan <2642551760@qq.com>
2022-06-21 01:05:58 +0800

    完成了报表功能的开发

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/params.vue'
 import GoodsList from './components/goods/list.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -50,6 +51,9 @@ const router = new VueRouter({
                 },
                 {
                     path:'/orders',component:Order
+                },
+                {
+                    path:'/reports',component:Report
                 }
             ]
         }
